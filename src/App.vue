245198487270ecbcd7ecc2fba990af62b4f1c9e0
wangmaoquan <1782976211@qq.com>
2023-05-30 16:49:00 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  const count = ref(1);
+  const add = () => {
+    count.value++;
+  };
+  const add2 = () => {
+    throw Error('11');
+  };
+</script>
+
+<template>
+  <div>
+    <h1>{{ count }}</h1>
+    <div><button @click="add">add</button></div>
+    <div><button @click="add2">error</button></div>
+  </div>
+</template>
+
+<style scoped></style>
